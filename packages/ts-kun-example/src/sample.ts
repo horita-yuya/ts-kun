8c3a0ff14a2680b0cd0f4da19f7006a941f3d6d8
horita-yuya <horitayuya@gmail.com>
2024-09-04 15:40:32 +0900

    Simple

--- packages/ts-kun-example/src/sample.ts
+++ packages/ts-kun-example/src/sample.ts
@@ -3,11 +3,4 @@ export type User = {
   name: string;
 };
 
-export type Post = {
-  id: number;
-  title: string;
-  content: string;
-};
-
 export declare function validateUser(input: unknown): input is User;
-export declare function validatePost(input: unknown): input is Post;
